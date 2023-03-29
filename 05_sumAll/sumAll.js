c95d5ea54bfab18c35c40b99a8b3b6a26c4cb7cf
sk-rw <123981682+sk-rw@users.noreply.github.com>
2023-03-28 22:31:19 -0500

    Update

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0) {
+      return "ERROR";
+    }
+    let sum = 0;
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+      if (typeof i !== 'number') {
+        return "ERROR";
+      }
+      sum += i;
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
